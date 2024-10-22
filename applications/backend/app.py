7e7f699023ba19111646cb4854a319ad4273b6cf
der <mr73367@gmail.com>
2024-10-22 21:44:52 +0700

    update for backend

--- applications/backend/app.py
+++ applications/backend/app.py
@@ -5,7 +5,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 def get_db_connection():
-    conn = psycopg2.connect(
+    conn = psycopg2.connect( 
         host=os.environ['DB_HOST'],
         database=os.environ['DB_NAME'],
         user=os.environ['DB_USER'],
