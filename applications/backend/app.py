6ae627f4f74c6b2d6ff622b2fd5cdd8796befcc1
der <mr73367@gmail.com>
2024-10-23 20:08:14 +0700

    update

--- applications/backend/app.py
+++ applications/backend/app.py
@@ -20,6 +20,6 @@ def get_connection_info():
     print(connection_info)
     # Convert the connection info to a JSON response
     return jsonify(connection_info)
- 
+   
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
