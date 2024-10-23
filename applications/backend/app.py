f30ce3f181bccc4a91c3da588ff7feab41f74b00
der <mr73367@gmail.com>
2024-10-23 20:19:27 +0700

    update

--- applications/backend/app.py
+++ applications/backend/app.py
@@ -5,7 +5,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
    
 def get_db_connection():
-    conn = psycopg2.connect(    
+    conn = psycopg2.connect( 
         host=os.environ['DB_HOST'],
         database=os.environ['DB_NAME'],
         user=os.environ['DB_USER'],
@@ -14,9 +14,13 @@ def get_db_connection():
     return conn
 
 @app.route('/info')
-def get_connection_info():          
+def get_connection_info():   
+    # Get the connection information
+    conn = get_db_connection()
     connection_info = conn.get_dsn_parameters()
+    print(connection_info)
+    # Convert the connection info to a JSON response
     return jsonify(connection_info)
-    
+   
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
